41bc8afd5f6c2132dc2892797c282d06d838b973
sohail <sohail1613@gmail.com>
2022-09-27 18:10:13 +0530

    user service implementation

--- src/main/java/com/socialmedia/webApp/serviceImplementation/UserServiceImplementation.java
+++ src/main/java/com/socialmedia/webApp/serviceImplementation/UserServiceImplementation.java
@@ -1,12 +1,16 @@
 package com.socialmedia.webApp.serviceImplementation;
 
+import com.socialmedia.webApp.entity.User;
+import com.socialmedia.webApp.exception.ResourceNotFoundException;
 import com.socialmedia.webApp.payload.UserDto;
 import com.socialmedia.webApp.repositories.UserRepository;
 import com.socialmedia.webApp.services.UserServices;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImplementation implements UserServices {
@@ -14,28 +18,58 @@ public class UserServiceImplementation implements UserServices {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public UserDto createUser(UserDto userDto) {
-        return null;
+        User user = this.dtoToUser(userDto);
+        User userSaved = this.userRepository.save(user);
+        return this.userToDto(userSaved);
     }
 
     @Override
     public UserDto updateUser(UserDto userDto, Long userId) {
-        return null;
+        User user = this.userRepository.findById(userId)
+                .orElseThrow(()->new ResourceNotFoundException("User", "userId", userId));
+        user.setUserName(userDto.getUserName());
+        user.setUserEmail(user.getUserEmail());
+        User updatedUser = this.userRepository.save(user);
+        UserDto userDto1 = this.userToDto(updatedUser);
+        return userDto1;
     }
 
     @Override
     public void deleteUser(Long userId) {
-
+        User user = this.userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User", "userId", userId));
+        this.userRepository.delete(user);
     }
 
     @Override
     public UserDto getUserById(Long userId) {
-        return null;
+        User user = this.userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User", "userId", userId));
+        return this.userToDto(user);
     }
 
     @Override
     public List<UserDto> getAllUser() {
-        return null;
+        List<User> users = this.userRepository.findAll();
+        List<UserDto> userDtos = users.stream().map(user -> this.userToDto(user)).collect(Collectors.toList());
+        return userDtos;
+    }
+
+
+    //converting from Dto to user
+    private User dtoToUser(UserDto userDto){
+        User user = this.modelMapper.map(userDto, User.class);
+        return user;
+    }
+
+    //converting from user to dto
+    private UserDto userToDto(User user){
+        UserDto userDto = this.modelMapper.map(user, UserDto.class);
+        return userDto;
     }
 }
