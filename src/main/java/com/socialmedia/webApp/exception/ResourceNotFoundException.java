a7675f612ffa53cd10631b4d94632b2d76b52d6f
vishnu <vishnunitjsr@gmail.com>
2022-09-29 22:59:07 +0530

    not any changes

--- src/main/java/com/socialmedia/webApp/exception/ResourceNotFoundException.java
+++ src/main/java/com/socialmedia/webApp/exception/ResourceNotFoundException.java
@@ -6,9 +6,55 @@ public class ResourceNotFoundException extends RuntimeException{
     long fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue){
-        super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue));
+        super(String.format("%s not found with %s : %1", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
     }
+    
+    public String getResourceName() {
+		return resourceName;
+	}
+
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public long getFieldValue() {
+		return fieldValue;
+	}
+
+	public void setFieldValue(long fieldValue) {
+		this.fieldValue = fieldValue;
+	}
+
+	public ResourceNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
 }
